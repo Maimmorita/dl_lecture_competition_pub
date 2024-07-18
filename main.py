720a3db928460b976901a0b51b8c49703a79fdcb
Mai Morita <145355130+Maimmorita@users.noreply.github.com>
2024-07-18 11:04:23 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,117 +1,69 @@
-import os, sys
-import numpy as np
 import torch
+import torch.nn as nn
 import torch.nn.functional as F
-from torchmetrics import Accuracy
-import hydra
-from omegaconf import DictConfig
-import wandb
-from termcolor import cprint
-from tqdm import tqdm
+from einops.layers.torch import Rearrange
 
-from src.datasets import ThingsMEGDataset
-from src.models import BasicConvClassifier
-from src.utils import set_seed
+class BasicConvClassifier(nn.Module):
+    def __init__(
+        self,
+        num_classes: int,
+        seq_len: int,
+        in_channels: int,
+        hid_dim: int = 128
+    ) -> None:
+        super().__init__()
 
+        self.blocks = nn.Sequential(
+            ConvBlock(in_channels, hid_dim),
+            ConvBlock(hid_dim, hid_dim),
+        )
 
-@hydra.main(version_base=None, config_path="configs", config_name="config")
-def run(args: DictConfig):
-    set_seed(args.seed)
-    logdir = hydra.core.hydra_config.HydraConfig.get().runtime.output_dir
-    
-    if args.use_wandb:
-        wandb.init(mode="online", dir=logdir, project="MEG-classification")
+        self.head = nn.Sequential(
+            nn.AdaptiveAvgPool1d(1),
+            Rearrange("b d 1 -> b d"),
+            nn.Linear(hid_dim, num_classes),
+        )
 
-    # ------------------
-    #    Dataloader
-    # ------------------
-    loader_args = {"batch_size": args.batch_size, "num_workers": args.num_workers}
-    
-    train_set = ThingsMEGDataset("train", args.data_dir)
-    train_loader = torch.utils.data.DataLoader(train_set, shuffle=True, **loader_args)
-    val_set = ThingsMEGDataset("val", args.data_dir)
-    val_loader = torch.utils.data.DataLoader(val_set, shuffle=False, **loader_args)
-    test_set = ThingsMEGDataset("test", args.data_dir)
-    test_loader = torch.utils.data.DataLoader(
-        test_set, shuffle=False, batch_size=args.batch_size, num_workers=args.num_workers
-    )
+    def forward(self, X: torch.Tensor) -> torch.Tensor:
+        X = X.float()
+        X = self.blocks(X)
+        return self.head(X)
 
-    # ------------------
-    #       Model
-    # ------------------
-    model = BasicConvClassifier(
-        train_set.num_classes, train_set.seq_len, train_set.num_channels
-    ).to(args.device)
-
-    # ------------------
-    #     Optimizer
-    # ------------------
-    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
-
-    # ------------------
-    #   Start training
-    # ------------------  
-    max_val_acc = 0
-    accuracy = Accuracy(
-        task="multiclass", num_classes=train_set.num_classes, top_k=10
-    ).to(args.device)
-      
-    for epoch in range(args.epochs):
-        print(f"Epoch {epoch+1}/{args.epochs}")
+class ConvBlock(nn.Module):
+    def __init__(
+        self,
+        in_dim,
+        out_dim,
+        kernel_size: int = 3,
+        p_drop: float = 0.1,
+    ) -> None:
+        super().__init__()
         
-        train_loss, train_acc, val_loss, val_acc = [], [], [], []
-        
-        model.train()
-        for X, y, subject_idxs in tqdm(train_loader, desc="Train"):
-            X, y = X.to(args.device), y.to(args.device)
+        self.in_dim = in_dim
+        self.out_dim = out_dim
 
-            y_pred = model(X)
-            
-            loss = F.cross_entropy(y_pred, y)
-            train_loss.append(loss.item())
-            
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-            
-            acc = accuracy(y_pred, y)
-            train_acc.append(acc.item())
+        self.conv0 = nn.Conv1d(in_dim, out_dim, kernel_size, padding="same")
+        self.conv1 = nn.Conv1d(out_dim, out_dim, kernel_size, padding="same")
 
-        model.eval()
-        for X, y, subject_idxs in tqdm(val_loader, desc="Validation"):
-            X, y = X.to(args.device), y.to(args.device)
-            
-            with torch.no_grad():
-                y_pred = model(X)
-            
-            val_loss.append(F.cross_entropy(y_pred, y).item())
-            val_acc.append(accuracy(y_pred, y).item())
+        self.batchnorm0 = nn.BatchNorm1d(num_features=out_dim)
+        self.batchnorm1 = nn.BatchNorm1d(num_features=out_dim)
 
-        print(f"Epoch {epoch+1}/{args.epochs} | train loss: {np.mean(train_loss):.3f} | train acc: {np.mean(train_acc):.3f} | val loss: {np.mean(val_loss):.3f} | val acc: {np.mean(val_acc):.3f}")
-        torch.save(model.state_dict(), os.path.join(logdir, "model_last.pt"))
-        if args.use_wandb:
-            wandb.log({"train_loss": np.mean(train_loss), "train_acc": np.mean(train_acc), "val_loss": np.mean(val_loss), "val_acc": np.mean(val_acc)})
-        
-        if np.mean(val_acc) > max_val_acc:
-            cprint("New best.", "cyan")
-            torch.save(model.state_dict(), os.path.join(logdir, "model_best.pt"))
-            max_val_acc = np.mean(val_acc)
-            
-    
-    # ----------------------------------
-    #  Start evaluation with best model
-    # ----------------------------------
-    model.load_state_dict(torch.load(os.path.join(logdir, "model_best.pt"), map_location=args.device))
+        self.dropout = nn.Dropout(p_drop)
 
-    preds = [] 
-    model.eval()
-    for X, subject_idxs in tqdm(test_loader, desc="Validation"):        
-        preds.append(model(X.to(args.device)).detach().cpu())
-        
-    preds = torch.cat(preds, dim=0).numpy()
-    np.save(os.path.join(logdir, "submission"), preds)
-    cprint(f"Submission {preds.shape} saved at {logdir}", "cyan")
+    def forward(self, X: torch.Tensor) -> torch.Tensor:
+        if self.in_dim == self.out_dim:
+            residual = X
+        else:
+            residual = 0
 
+        X = self.conv0(X)
+        X = self.batchnorm0(X)
+        X = F.gelu(X)
 
-if __name__ == "__main__":
-    run()
+        X = self.conv1(X)
+        X = self.batchnorm1(X)
+        X = F.gelu(X)
+
+        X += residual  # skip connection
+
+        return self.dropout(X)
